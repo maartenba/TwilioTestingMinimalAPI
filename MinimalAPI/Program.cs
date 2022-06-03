f8129e0e466c989921e401d5d0eac890dd4120c2
Maarten Balliauw <maarten@maartenballiauw.be>
2022-06-03 11:02:21 +0200

    Implement /people endpoint

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,8 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using MiniValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IPeopleService, PeopleService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/people", (Person person, IPeopleService peopleService) =>
+    !MiniValidator.TryValidate(person, out var errors)
+        ? Results.ValidationProblem(errors)
+        : Results.Ok(peopleService.Create(person)));
+
 app.Run();
 
 public partial class Program { }
+
+public interface IPeopleService
+{
+    string Create(Person person);
+}
+
+public class PeopleService : IPeopleService
+{
+    public string Create(Person person) 
+        => $"Hello, {person.FirstName} {person.LastName}";
+}
+
+public class Person
+{
+    [Required, MinLength(2)]
+    public string? FirstName { get; set; }
+
+    [Required, MinLength(2)]
+    public string? LastName { get; set; }
+
+    [Required, DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+}
